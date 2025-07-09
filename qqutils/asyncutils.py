561440fd028fcbb4d74e400b803fc495c9106e56
Hao Ruan <ruanhao1116@gmail.com>
2025-07-09 19:18:50 +0800

    add description for wait_for_complete

--- qqutils/asyncutils.py
+++ qqutils/asyncutils.py
@@ -21,7 +21,12 @@ class _DummyTqdm:
         pass
 
 
-def wait_for_complete(*coroutines: Awaitable[Any], progress: bool = False) -> list[Any | Exception]:
+def wait_for_complete(
+        *coroutines: Awaitable[Any],
+        progress: bool = False,
+        ignore_exceptions: bool = False,
+        description="Running coroutines"
+) -> list[Any | Exception]:
     _tqdm = _DummyTqdm if not progress else tqdm
 
     async def __wrapper(coroutine: Awaitable[Any], pbar: tqdm) -> Any:
@@ -31,9 +36,12 @@ def wait_for_complete(*coroutines: Awaitable[Any], progress: bool = False) -> li
             pbar.update(1)
 
     async def __wait_for_tasks() -> list[Any]:
-        with _tqdm(total=len(coroutines), desc="Running coroutines") as pbar:
+        with _tqdm(total=len(coroutines), desc=description) as pbar:
             coroutines_ = [__wrapper(coroutine, pbar) for coroutine in coroutines]
-            return await asyncio.gather(*coroutines_, return_exceptions=True)
+            result = await asyncio.gather(*coroutines_, return_exceptions=True)
+            if ignore_exceptions:
+                return [r for r in result if not isinstance(r, Exception)]
+            return result
 
     loop = asyncio.get_event_loop()
     return loop.run_until_complete(__wait_for_tasks())
--- tests/test_asyncutils.py
+++ tests/test_asyncutils.py
@@ -40,3 +40,19 @@ def test_wait_for_complete_case_error():
     print(results)
     assert len(results) == 5, f"Expected 5 results, got {len(results)}"
     assert isinstance(results[2], ValueError), "Expected an exception for the third task"
+
+
+def test_wait_for_complete_case_ignore_error():
+    results = wait_for_complete(
+        sleep(1),
+        sleep(2),
+        throw_error(),
+        sleep(3),
+        sleep(4),
+        progress=True,
+        ignore_exceptions=True,
+    )
+    print(results)
+    assert len(results) == 4, f"Expected 4 results, got {len(results)}"
+    for result in results:
+        assert not isinstance(result, Exception), "Expected no exceptions in results"
